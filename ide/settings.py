f74090040a2359b8911de895b9f4783f5372f44e
Mohit Garg <mohitgarg0795@gmail.com>
2016-03-20 23:15:51 +0530

    settings revised

--- ide/settings.py
+++ ide/settings.py
@@ -128,11 +128,11 @@ DATABASES['default'].update(db_from_env)
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'page/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'page/static'),
 )
 
 # Simplified static file serving.
